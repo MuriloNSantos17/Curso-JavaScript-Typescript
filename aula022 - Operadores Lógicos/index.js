0e986812e7f790b14c3694ee62b98d7a4f3f3dc6
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-08-30 22:47:25 -0300

    Commit aula022

---
+++ aula022 - Operadores Lógicos/index.js
@@ -0,0 +1,18 @@
+
+//AND
+console.log(5<10 && 5<10);
+
+//OR
+console.log(5<10 || 5<10);
+
+//NOT
+console.log(!5>10);
+
+//Example with String 
+
+var usuario = "Murilo Santos";
+var senha="12345";
+
+var resultado = usuario==="Murilo Santos" && senha==="123456";
+
+console.log(`Logou ${resultado}`);
