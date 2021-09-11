21e6b731dd20ecc3ca83db902fc51ca3e9c8753c
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-09-11 18:24:18 -0300

    Commit aula037

---
+++ aula037 - For Of/index.js
@@ -0,0 +1,12 @@
+const nome = 'Murilo Santos';
+
+for(let i of nome)
+{
+    console.log(i);
+}
+
+const nomes = ['Sara','Nataly','Wallysson'];
+
+nomes.forEach(function(valor,indice){
+    console.log(valor,indice);
+});
