51f22d2af803929b83f799128f0134e86862083a
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-09-11 17:57:11 -0300

    Commit aula 036

---
+++ aula036 - For In/index.js
@@ -0,0 +1,18 @@
+const frutas = ['Pera','Maçã','Uva'];
+
+for(let i in frutas)
+{
+    console.log(frutas[i]);
+}
+
+const pessoa={
+    nome: "Murilo",
+    sobrenome: "Santos",
+    idade: 22,
+
+};
+
+for(let chave in pessoa)
+{
+    console.log(pessoa[chave]);
+}
