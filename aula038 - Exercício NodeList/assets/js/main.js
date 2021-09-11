fb2b7ab3afbb97a9d72255d71ea847db6b01cbc1
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-09-11 18:47:12 -0300

    Commit aula038

---
+++ aula038 - Exercício NodeList/assets/js/main.js
@@ -0,0 +1,19 @@
+const divParagrafos = document.querySelector('.paragrafos');
+
+//GETALL PARAGRAFOS DA DIV
+const tagsP = divParagrafos.querySelectorAll('p');
+
+//PEGA TODOS ESTILOS DO BODY
+const stylesBody = getComputedStyle(document.body);
+
+//PEGANDO O BACKGROUND DO BODY
+const backgroundColor = stylesBody.backgroundColor;
+
+//console.log(backgroundColor);
+
+
+for(p of tagsP)
+{
+    p.style.backgroundColor=backgroundColor;
+    p.style.color='White';
+}
