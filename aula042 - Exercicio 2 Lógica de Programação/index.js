081e6f84289fb5a1b1e3b7dbc215fab462ddf369
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-09-11 21:10:10 -0300

    Commit aula042

---
+++ aula042 - Exercicio 2 Lógica de Programação/index.js
@@ -0,0 +1,14 @@
+//Faça um programa que valide se uma imagem é paisagem ou retrato
+
+function isPaisagem(largura,altura)
+{
+    return largura>altura;
+}
+
+
+
+console.log(isPaisagem(500,700));
+
+const resultado = (a,l)=> a>l ;
+
+console.log(resultado(200,300));
