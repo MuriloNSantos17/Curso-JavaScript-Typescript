687cf4d64243e2e64579a7a66b9e260c3a7027bc
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-12-14 22:15:45 -0300

    Commit cpf

--- aula76 - Validando CPF/index.js
+++ aula76 - Validando CPF/index.js
@@ -18,7 +18,7 @@
 
     Repetimos a conta com outra lógica, começando em 11 e indo para o primeiro digito no final
 
-    7 x 11 = 70
+    7 x 11 = 7
     0 x 10 = 0
     5 x 09 = 40
     4 x 08 = 28 
@@ -29,3 +29,61 @@
     0 X 03 = 0
     Digito 1 * 02 = Resultado = 11 - (284 % 11) =2
 */
+function validaCpf(cpf){
+
+    console.log(cpf);
+
+    let numeroCpf = cpf;
+
+
+    numeroCpf = numeroCpf.replace('-','');
+    numeroCpf = numeroCpf.replace('.','');
+    numeroCpf = numeroCpf.replace('.','');
+
+    console.log(numeroCpf);
+    
+
+    let somaPrimeiroDigito=0;
+    let contadorInterno=9;
+
+    for(i=2; i<=10; i++)
+    {           
+        somaPrimeiroDigito += ( i * numeroCpf[contadorInterno] );
+        contadorInterno--;
+    }
+
+    let primeiroDigito = 11 - (somaPrimeiroDigito %11);
+
+    if(primeiroDigito > 9)
+    {
+        primeiroDigito =0;
+    }
+
+    contadorInterno =8;
+
+    let somaSegundoDigito=0;
+    for(i=3; i<=11; i++)
+    {
+        console.log(numeroCpf[contadorInterno]);
+
+        somaSegundoDigito += ( i * numeroCpf[contadorInterno]);
+
+        contadorInterno--;
+    }
+
+    somaSegundoDigito += (primeiroDigito *2);
+
+    let segundoDigito = 11 - (somaSegundoDigito %11);
+
+    console.log('Somas')
+    console.log(somaPrimeiroDigito);
+    console.log(somaSegundoDigito); 
+
+    console.log('Digitos');
+    console.log(primeiroDigito);
+    console.log(segundoDigito);
+}
+
+const cpf = '235.144.580-57';
+
+validaCpf(cpf);
