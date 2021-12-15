9dbd8c10f5e3a2181db18aa6799600f8e52ab384
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-12-14 21:49:25 -0300

    Commit comentários

---
+++ aula76 - Validando CPF/index.js
@@ -0,0 +1,31 @@
+/*
+    705.484.450-52
+
+    7 x 10 = 70
+    0 x 09 = 0
+    5 x 08 = 40
+    4 x 07 = 28 
+    8 x 06 = 48 
+    4 X 05 = 20
+    4 X 04 = 16 
+    5 x 03 = 15 
+    0 X 02 = 0 
+    Soma = 237;
+
+    Resultado = (237 % 11) = 5 (Primeiro Digito)
+
+    se o número do  digito for maior que 9 consideramos 0
+
+    Repetimos a conta com outra lógica, começando em 11 e indo para o primeiro digito no final
+
+    7 x 11 = 70
+    0 x 10 = 0
+    5 x 09 = 40
+    4 x 08 = 28 
+    8 x 07 = 48 
+    4 X 06 = 20
+    4 X 05 = 16 
+    5 x 04 = 15 
+    0 X 03 = 0
+    Digito 1 * 02 = Resultado = 11 - (284 % 11) =2
+*/
