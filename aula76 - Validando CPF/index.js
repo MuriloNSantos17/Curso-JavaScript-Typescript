8d2cd100610c0514378f46a0af35cb758df67948
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2022-01-11 20:58:45 -0300

    Commit validar CPF

--- aula76 - Validando CPF/index.js
+++ aula76 - Validando CPF/index.js
@@ -1,89 +1,77 @@
 /*
     705.484.450-52
 
-    7 x 10 = 70
-    0 x 09 = 0
-    5 x 08 = 40
-    4 x 07 = 28 
-    8 x 06 = 48 
-    4 X 05 = 20
-    4 X 04 = 16 
-    5 x 03 = 15 
-    0 X 02 = 0 
-    Soma = 237;
-
-    Resultado = (237 % 11) = 5 (Primeiro Digito)
-
-    se o número do  digito for maior que 9 consideramos 0
-
-    Repetimos a conta com outra lógica, começando em 11 e indo para o primeiro digito no final
-
-    7 x 11 = 7
-    0 x 10 = 0
-    5 x 09 = 40
-    4 x 08 = 28 
-    8 x 07 = 48 
-    4 X 06 = 20
-    4 X 05 = 16 
-    5 x 04 = 15 
-    0 X 03 = 0
-    Digito 1 * 02 = Resultado = 11 - (284 % 11) =2
+    07X  00X 05X 04X 08X 04X 04X 05X 000X
+    010  009 008 007 006 005 004 003 0002 
 */
-function validaCpf(cpf){
-
-    console.log(cpf);
-
-    let numeroCpf = cpf;
 
 
-    numeroCpf = numeroCpf.replace('-','');
-    numeroCpf = numeroCpf.replace('.','');
-    numeroCpf = numeroCpf.replace('.','');
+/*
+    705.484.450-52
 
-    console.log(numeroCpf);
+    07X  00X 05X 04X 08X 04X 04X 05X 000X 1dgt
+    011  010 009 008 007 006 005 004 0003 0002
+*/
+
+
+function validaCPF(numeroCpf)
+{
+    let contadorInterno=8;
+    var calc=0;
+    var soma=0;
+
+    for(i=2;i<=10;i++)
+    {
+        //console.log("I: "+i)
+        //console.log(numeroCpf[contadorInterno])
+
+        calc = Number(numeroCpf[contadorInterno]) * i;
+        
+        soma+=calc;
+        
+        contadorInterno--;
+    }
+
+    //console.log(soma);
     
+    var primeiroDigito = 11 - (soma % 11);
 
-    let somaPrimeiroDigito=0;
-    let contadorInterno=9;
-
-    for(i=2; i<=10; i++)
-    {           
-        somaPrimeiroDigito += ( i * numeroCpf[contadorInterno] );
-        contadorInterno--;
-    }
-
-    let primeiroDigito = 11 - (somaPrimeiroDigito %11);
-
-    if(primeiroDigito > 9)
+    if(primeiroDigito >9)
     {
-        primeiroDigito =0;
+        primeiroDigito=0;
     }
+    console.log("*****")
 
-    contadorInterno =8;
+    contadorInterno =11;
+    soma=0;
 
-    let somaSegundoDigito=0;
-    for(i=3; i<=11; i++)
+    for(i=0;i<=8;i++)
     {
-        console.log(numeroCpf[contadorInterno]);
+        console.log("C: "+contadorInterno)
+        console.log(numeroCpf[i])
 
-        somaSegundoDigito += ( i * numeroCpf[contadorInterno]);
-
+        calc = Number(numeroCpf[i]) * contadorInterno;
+        
         contadorInterno--;
+
+        soma+=calc;
+        
+        
     }
 
-    somaSegundoDigito += (primeiroDigito *2);
+    var segundoDigito =0;
+    
+    soma += (primeiroDigito*2);
 
-    let segundoDigito = 11 - (somaSegundoDigito %11);
+    segundoDigito = 11 - (soma % 11);
 
-    console.log('Somas')
-    console.log(somaPrimeiroDigito);
-    console.log(somaSegundoDigito); 
-
-    console.log('Digitos');
+    if(segundoDigito>9)
+    {
+        segundoDigito = 0;
+    }
+    
     console.log(primeiroDigito);
     console.log(segundoDigito);
 }
 
-const cpf = '235.144.580-57';
-
-validaCpf(cpf);
+validaCPF('70548445052');
