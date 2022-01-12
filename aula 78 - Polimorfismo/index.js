cd7768ec45315b57a219bfaf1c48739baaf1e639
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2022-01-12 20:47:00 -0300

    Commit conta corrente

--- aula 78 - Polimorfismo/index.js
+++ aula 78 - Polimorfismo/index.js
@@ -9,8 +9,8 @@ function CriaConta(agencia, numeroConta, saldo)
 CriaConta.prototype.sacar = function(valor){
     if(valor > this.saldo)
     {
-        console.log("Saldo Insuficiente!");
-        this.verSaldo();
+        console.log(`Saldo Insuficiente!\nSaldo Atual ${this.saldo}`);
+        //this.verSaldo();
         return;
     }
 
@@ -30,4 +30,33 @@ CriaConta.prototype.verSaldo = function()
 
 const conta1 = new CriaConta(10,236,10);
 conta1.depositar(20);
-conta1.sacar(30);
+conta1.sacar(31);
+
+function ContaCorrente(agencia,numeroConta,saldo,limite)
+{
+    CriaConta.call(this,agencia,numeroConta,saldo);
+
+    this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(CriaConta.prototype);
+ContaCorrente.prototype.constructor= ContaCorrente;
+
+
+ContaCorrente.prototype.sacar = function(valor){
+    if(valor > (this.saldo+this.limite))
+    {
+        console.log(`Saldo Insuficiente!\nSaldo Atual ${this.saldo}`);
+        //this.verSaldo();
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+}
+
+const conta2 = new ContaCorrente(23,963,100,200);
+console.log(conta2);
+
+conta2.sacar(400);
+
