521662635fdd4bf75baa984bfba44edda34e64b5
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2022-08-01 21:30:09 -0300

    Commit correção module

--- aula094 - Modules/src/index.js
+++ aula094 - Modules/src/index.js
@@ -1,1 +1,7 @@
-console.log('olá')
+import { nome, soma } from './modulo1.js';
+
+console.log(nome)
+
+var calc = soma(1,2);
+
+console.log(calc)
---
+++ aula094 - Modules/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = "Murilo";
+const sobrenome = "Santos";
+
+function soma(x,y)
+{
+    return x+y;
+}
+
+export { nome, sobrenome, soma };
