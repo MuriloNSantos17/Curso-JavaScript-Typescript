ce8a017cd986dcfce886a838e7d56cdae90e3b84
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2023-02-14 20:26:46 -0300

    Commit all

--- node/aula17/src/controllers/controlPages.js
+++
@@ -1,9 +0,0 @@
-function showFirstPage(req,res){
-    req.session.usuario = {nome: 'Murilo', logado: true}
-    res.render('index',{
-        //titulo: 'Este Será o Titulo da Página',
-        numeros: [1,2,3,4,5,6]
-    });
-}
-
-module.exports = {showFirstPage};
--- node/aula17/src/middleware/middleware.js
+++
@@ -1,7 +0,0 @@
-
-
-exports.checksCsrfError = (err,req,res,next)=>{
-    if(err && 'EBADCSRFTOKEN' === err.code){
-        return res.render('404')
-    }
-}
